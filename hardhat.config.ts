4f69641d390d090edb3195d127c4a4db7a1b4529
smartdoge <hello@smartdoge.cash>
2022-01-18 16:44:35 -0600

    remove compiler settings override

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -12,31 +12,6 @@ const config: HardhatUserConfig = {
         "useLiteralContent": true
       }
     },
-    overrides: {
-      "uniswapv2/*": {
-        version: "0.6.12",
-        settings: { 
-          "outputSelection": {
-            "*": {
-              "*": [
-                "evm.bytecode.object",
-                "evm.deployedBytecode.object",
-                "abi",
-                "evm.bytecode.sourceMap",
-                "evm.deployedBytecode.sourceMap",
-                "metadata"
-              ],
-              "": ["ast"]
-            }
-          },
-          "evmVersion": "istanbul",
-          "optimizer": {
-            "enabled": true,
-            "runs": 999999
-          }      
-        }
-      }
-    }
   },
   paths: {
     sources: 'src',
